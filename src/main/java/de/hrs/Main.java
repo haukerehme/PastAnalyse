3d90f353cd64da77437883fb17eafccc1f4d8757
Hauke Rehme-Schlüter <hrs@logentis.de>
2016-12-19 15:21:41 +0100

    db fix

--- src/main/java/de/hrs/Main.java
+++ src/main/java/de/hrs/Main.java
@@ -1,6 +1,7 @@
 package de.hrs;
 
 import de.hrs.controller.Analyse;
+import de.hrs.repair.FixDb;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,27 +18,34 @@ import java.util.GregorianCalendar;
 public class Main {
     public static void main(String[] args) throws ParseException {
         ApplicationContext context = new ClassPathXmlApplicationContext("jdbc-config.xml");
-        if (args.length == 2){
+
+//        if (args.length == 1 && args[0].equals("fix")){
+                context.getBean(FixDb.class).addDbFieldsCurrentValueAndFutureValue();
+        /*}else {
+
+            if (args.length == 2) {
 
 
-        }else if (args.length == 1){
-            //2015-01-01 18:29:00
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-            Timestamp timestamp = new Timestamp(dateFormat.parse(args[0]).getTime());
-            context.getBean(Analyse.class).startAnalyse(timestamp);
-        }else{
-            GregorianCalendar gc = new GregorianCalendar();
-            int year = randBetween(2009, 2015);
-            gc.set(gc.YEAR, year);
-            int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
-            gc.set(gc.DAY_OF_YEAR, dayOfYear);
+            } else if (args.length == 1) {
+                //2015-01-01 18:29:00
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                Timestamp timestamp = new Timestamp(dateFormat.parse(args[0]).getTime());
+                context.getBean(Analyse.class).startAnalyse(timestamp);
+            } else {
 
-            context.getBean(Analyse.class).startAnalyse(new Timestamp(gc.getTime().getTime()));
+                GregorianCalendar gc = new GregorianCalendar();
+                int year = randBetween(2009, 2015);
+                gc.set(gc.YEAR, year);
+                int dayOfYear = randBetween(1, gc.getActualMaximum(gc.DAY_OF_YEAR));
+                gc.set(gc.DAY_OF_YEAR, dayOfYear);
+
+                context.getBean(Analyse.class).startAnalyse(new Timestamp(gc.getTime().getTime()));
 
 //            System.out.println(gc.get(gc.YEAR) + "-" + (gc.get(gc.MONTH) + 1) + "-" + gc.get(gc.DAY_OF_MONTH));
 //
 //            System.out.println("Bitte geben Sie einen oder zwei Parameter an");
-        }
+            }
+        }*/
     }
 
     public static int randBetween(int start, int end) {
